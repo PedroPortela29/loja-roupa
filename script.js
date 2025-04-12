70d8fff78e4da09a8cce2cbd71d74885567b7b6a
PedroHAPortela <phaporte@gmail.com>
2025-04-12 10:57:01 -0300

    começando projeto novo

---
+++ script.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const swiper = new Swiper('.swiper-container', {
+      loop: true,
+      slidesPerView: 1,
+      spaceBetween: 20,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      breakpoints: {
+        768: {
+          slidesPerView: 2
+        },
+        1024: {
+          slidesPerView: 3
+        }
+      }
+    });
+  });
+  
